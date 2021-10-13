99123756e27b36a18fa7c2b4826c6e19e0783bc1
Sofie Wallin <sofie.wallin84@gmail.com>
2021-10-13 07:29:41 +0200

    Add Babel with loader and preset to enable transforms for ES2015+

--- webpack.common.js
+++ webpack.common.js
@@ -12,6 +12,16 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: "babel-loader",
+                  options: {
+                    presets: ['@babel/preset-env']
+                  }
+                }
+            },
+            {
                 test: /\.html$/i,
                 loader: 'html-loader'
             },
